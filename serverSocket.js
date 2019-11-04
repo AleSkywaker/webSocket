e17cbe20924f7a5bdd29732004640bb3dc2b9d9f
Alex Colombo <uricaine@hotmail.com>
2019-11-04 21:27:27 +0100

    install and using socketIO

--- serverSocket.js
+++ serverSocket.js
@@ -3,10 +3,21 @@ const http = require('http');
 var socketio = require('socket.io');
 
 const server = http.createServer((req, res) => {
-  res.end('I am server');
+  res.end('I am server 7000');
+});
+
+const io = socketio(server);
+
+io.on('connection', (socket, req) => {
+  // ws.send became socket.emit
+  socket.emit('welcome to websocket server!!! 👻');
+  socket.on('kaka', msg => {
+    console.log(msg);
+  });
 });
 
 const PORT = 7000;
+
 server.listen(PORT, () => {
   console.log(`Listen on port ${PORT}`);
 });
