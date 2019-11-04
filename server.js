b930c55b9de9547c6ab6f32d11c3254dfb02d0f5
Alex Colombo <uricaine@hotmail.com>
2019-11-04 21:08:22 +0100

    done server with ws

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const http = require('http');
 
-const ws = require('ws');
+const webSocket = require('ws');
 
 const server = http.createServer((req, res) => {
   res.end('I am server');
 });
 
-const PORT = 5000;
-server.listen(5000, () => {
+const webSocketServer = new webSocket.Server({ server });
+
+webSocketServer.on('headers', (headers, req) => {
+  console.log(headers);
+});
+
+webSocketServer.on('connection', (ws, req) => {
+  ws.send('Bienvenido al servidor de websocket');
+  ws.on('message', msg => {
+    console.log(msg);
+    ws.send(msg);
+  });
+  console.log(ws.send);
+});
+
+const PORT = 8000;
+server.listen(PORT, () => {
   console.log(`Listen on port ${PORT}`);
 });
