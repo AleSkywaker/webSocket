2ef2d0e0296f1183f44709cb4de0ddbaba8e3323
Alex Colombo <uricaine@hotmail.com>
2019-11-04 21:08:35 +0100

    done client with ws

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let ws = new WebSocket('ws://localhost:8000');
+console.log(ws);
+
+ws.onopen = event => {
+  ws.send('Hola!!! estoy tratando de comunicarme desde el mas alla');
+};
+ws.onmessage = event => {
+  document.write(event.data);
+  console.log(event);
+};
