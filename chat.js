86df8a91d7dc5477a03e68ccabc2a8c116e78a6b
Alex Colombo <uricaine@hotmail.com>
2019-11-07 17:44:40 +0100

    using middleware

--- chat.js
+++ chat.js
@@ -2,3 +2,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+
+let port = 6500;
+app.listen(port, () => {
+  console.log(`listen on port ${port}`);
+});
