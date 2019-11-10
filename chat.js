3d30997ce66fb9c7d868c610e15d00f96eb84c6e
Alex Colombo <uricaine@hotmail.com>
2019-11-10 21:15:37 +0100

    change from event name

--- chat.js
+++ chat.js
@@ -10,7 +10,7 @@ const expressServer = app.listen(6500, () => {
 });
 
 const io = socketio(expressServer);
-// we can use either connection or connect
+// we can use connection or connect
 io.on('connect', socket => {
   socket.emit('mensajeDesdeServidor', { datos: 'datos desde el server 🦄' });
   socket.on('datosAlServidor', datosDelCliente => {
