29bd6566b202e821a56f2c22bcd046244bde47c0
Alex Colombo <uricaine@hotmail.com>
2019-11-10 20:56:31 +0100

    new message

--- public/chatCliente.js
+++ public/chatCliente.js
@@ -2,5 +2,5 @@ const socket = io('http://localhost:6500/');
 socket.on('mensajeDesdeServidor', datosDesdeServidor => {
   console.log(datosDesdeServidor);
   document.write(datosDesdeServidor.datos);
-  socket.emit('datosAlServidor', { datos: 'Datos al servidor' });
+  socket.emit('datosAlServidor', { datosToServer: 'Se acaba de conectar un nuevo cliente' });
 });
