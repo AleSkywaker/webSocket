7a71b0f5eee8b1ceaa19f99469d6d66cbf472700
Alex Colombo <uricaine@hotmail.com>
2019-11-14 21:10:56 +0100

    new connection

--- public/chatCliente.js
+++ public/chatCliente.js
@@ -1,20 +1,32 @@
-const titleElement = document.getElementById('title');
-const socket = io('http://localhost:6500/');
-socket.on('mensajeDesdeServidor', datosDesdeServidor => {
+const titleElement = document.getElementById("title");
+const socket = io("http://localhost:6500/");
+socket.on("mensajeDesdeServidor", datosDesdeServidor => {
   console.log(datosDesdeServidor);
-  titleElement.innerHTML = datosDesdeServidor.datos;
+  //titleElement.innerHTML = datosDesdeServidor.datos;
   // document.write(datosDesdeServidor.datos);
-  socket.emit('datosAlServidor', { datosToServer: 'Se acaba de conectar un nuevo cliente' });
+  socket.emit("datosAlServidor", {
+    datosToServer: "Se acaba de conectar un nuevo cliente"
+  });
 });
 
-socket.on('ping', () => {
-  console.log('Ping was received from server');
+document.querySelector("#message-form").addEventListener("submit", e => {
+  e.preventDefault();
+  const newMessage = document.querySelector("#user-message").value;
+  console.log("Form submitted!!!", newMessage);
+  socket.emit("mensajeDesdeCliente", { texto: newMessage });
+});
+socket.on("ping", () => {
+  console.log("Ping was received from server");
 });
 
-socket.on('pong', latency => {
+socket.on("pong", latency => {
   console.log(latency);
-  console.log('pong to server');
+  console.log("pong to server");
 });
-socket.on('connect', () => {
+socket.on("connect", () => {
   console.log(socket.id);
+});
+
+socket.on("sendDAtos", d => {
+  document.getElementById("messages").innerHTML = d.datosDelServer.texto;
 });
